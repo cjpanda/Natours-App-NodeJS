e058e604392f744984d92fffb117a722fd6a101b
cjpanda <verifiedceejay@gmail.com>
2025-11-09 10:36:44 +0100

    Update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -169,7 +169,23 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $group: {
           _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
         },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
       },
     ]);
 
